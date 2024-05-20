c63bc4814d08bcd1a041fc8f98c0c7e72ce092b6
jerrylc.liu <jerrylc.liu@dji.com>
2024-05-20 19:31:42 +0800

    fix: 改用UIDevice的orientation

--- ios/Classes/DisplayOrientationListener.swift
+++ ios/Classes/DisplayOrientationListener.swift
@@ -25,23 +25,24 @@ class DisplayOrientationListener: OrientationListener {
     return nil
   }
   
-  private func getDeviceOrientation() -> String {
-    switch (UIApplication.shared.statusBarOrientation) {
-    case .portrait:
-      return PORTRAIT_UP
-    case .portraitUpsideDown:
-      return PORTRAIT_DOWN
-    case .landscapeRight:
-      // return left for right as UIInterfaceOrientation is 'the amount needed
-      // to rotate to get back to normal', not the actual rotation
-      return LANDSCAPE_LEFT
-    case .landscapeLeft:
-      // return right for left, see above
-      return LANDSCAPE_RIGHT
-    default:
-      return UNKNOWN
+    private func getDeviceOrientation() -> String {
+        let deviceOrientation = UIDevice.current.orientation
+        switch (deviceOrientation) {
+        case .portrait:
+          return PORTRAIT_UP
+        case .portraitUpsideDown:
+          return PORTRAIT_DOWN
+        case .landscapeLeft:
+          // return left for right as UIInterfaceOrientation is 'the amount needed
+          // to rotate to get back to normal', not the actual rotation
+          return LANDSCAPE_LEFT
+        case .landscapeRight:
+          // return right for left, see above
+          return LANDSCAPE_RIGHT
+        default:
+          return UNKNOWN
+        }
     }
-  }
   
   @objc private func receiveOrientationChange() {
     let orientation = getDeviceOrientation()
